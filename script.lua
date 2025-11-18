ecb08513148391ca421fc610c8ed1e6345c501f8
vitor396 <vitorsdutra@icloud.com>
2025-11-17 23:42:51 -0300

    Update script.lua

--- script.lua
+++ script.lua
@@ -7,7 +7,7 @@ local Window = redzlib:MakeWindow({
 })
 
 Window:AddMinimizeButton({
-    Button = { Image = "rbxassetid://124917680185900", BackgroundTransparency = 1 },
+    Button = { Image = "rbxassetid://1438325722019266700", BackgroundTransparency = 1 },
     Corner = { CornerRadius = UDim.new(0, 6) },
 })
 
@@ -16,7 +16,7 @@ local Tab1 = Window:MakeTab({"Discord", "Info"})
 Tab1:AddDiscordInvite({
     Name = "StarVerse | Community",
     Description = "Entre pra receber atualizações sobre o script!",
-    Logo = "rbxassetid://1429259070707073097",
+    Logo = "rbxassetid://1438325722019266700",
     Invite = "https://discord.gg/C47PMD64f",
 })
 
@@ -194,6 +194,7 @@ TabFarm:AddToggle({
         end
     end
 })
+
 
 
 
